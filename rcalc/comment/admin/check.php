68a3ccaf73bbb1eeb796b8af0a45a599f51d3464
Xavier de Garay <degaray@yahoo.com>
2014-11-04 06:58:17 -0600

    deleted non-used files

--- rcalc/comment/admin/check.php
+++
@@ -1,7 +0,0 @@
-<?
-if(!(isset($_SESSION['userid']) and strlen($_SESSION['userid']) > 2)){
-echo "Please <a href=login.php>login</a> to use this page ";
-exit;
-}
-
-?>
--- rcalc/comment/admin/menu.php
+++
@@ -1,4 +0,0 @@
-<?
-echo "<a href=list.php>List All</a> | <a href=list-napv.php>Not Apv only</a>
-<br>";
-?>
